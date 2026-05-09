84eedf7ccae3c9470b30be890751b249da827f69
Luis Chang <luis.chang@outlook.com>
2026-05-09 11:08:33 -0500

    Implement country ranking list on Home screen
    - Add `CountryModel` data class to represent country information.
    - Add `coil-compose` dependency for asynchronous image loading.
    - Grant `INTERNET` permission in `AndroidManifest.xml`.
    - Update `HomeScreen` to display a `LazyColumn` of countries using a mock data list.
    - Implement country list items using `Card`, `Row`, and Coil's `rememberAsyncImagePainter`.
    - Update project configuration files (`misc.xml`, `gradle.xml`).

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,4 +58,6 @@ dependencies {
 
     //Navigation Compose
     implementation("androidx.navigation:navigation-compose:2.9.8")
+    //Coil compose
+    implementation("io.coil-kt:coil-compose:2.7.0")
 }
